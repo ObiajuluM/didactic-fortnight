b5a80158fa997880d5b57aa1b806ddb903cbb823
ObiajuluM <oambanefo@gmail.com>
2026-01-07 05:58:41 +0100

    Forms++
    Add employment status and dietary restrictions fields to sign-up form

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,11 @@ function App() {
   function signUp(formData) {
     const email = formData.get("email");
     const password = formData.get("password");
+    const employmentStatus = formData.get("employmentStatus");
+    const dietaryRestrictions = formData.getAll("dietaryRestrictions"); // getAll for checkboxes to get all selected values
 
-    console.log({ email, password }); // object shorthand to log an object with those two values.
+    console.log({ email, password, employmentStatus }); // object shorthand to log an object with those two values.
+    console.log(dietaryRestrictions);
   }
 
   return (
@@ -46,6 +49,7 @@ function App() {
           // to access the value of the input, we need to add a
           name="email"
           placeholder="joe@schmoe.com"
+          defaultValue="joe@schmoe.com" // pre-filling the input with a value
         />
         <br />
 
@@ -53,13 +57,73 @@ function App() {
         <input
           id="password"
           type="password"
-          // to access the value of the input, we need to add a
+          // to access the value of the input, we need to add a name
           name="password"
           placeholder="Enter your password"
         />
         <br />
 
         <button type="submit">Sign Up</button>
+
+        {/* text area example for bigger inputs*/}
+        <label htmlFor=""></label>
+        <textarea name="" id="" defaultValue="this is some text"></textarea>
+
+        {/* creates the black border and supports label on border */}
+        <fieldset>
+          <legend>Emplyoment Status</legend>
+
+          {/* radio: same name makes for single select*/}
+          <label htmlFor="">
+            <input
+              type="radio"
+              name="employmentStatus"
+              value="employed" // to access the value of the input we need to add a value
+            />
+            employed
+          </label>
+          <label htmlFor="">
+            <input type="radio" name="employmentStatus" value="unemployed" />
+            unemployed
+          </label>
+          <label htmlFor="">
+            <input
+              type="radio"
+              name="employmentStatus"
+              value="student"
+              defaultChecked={true} // pre-selecting this option
+            />
+            student
+          </label>
+        </fieldset>
+
+        {/* Checkbox */}
+        <fieldset>
+          <legend>Dietary Restrictions</legend>
+
+          {/* radio: same name makes for single select*/}
+          <label htmlFor="">
+            <input
+              type="checkbox"
+              name="dietaryRestrictions" // same name for grouping
+              value="beef" // to access the value of the input we need to add a value
+            />
+            beef
+          </label>
+          <label htmlFor="">
+            <input type="checkbox" name="dietaryRestrictions" value="pork" />
+            pork
+          </label>
+          <label htmlFor="">
+            <input
+              type="checkbox"
+              name="dietaryRestrictions"
+              value="kosher"
+              defaultChecked={true} // pre-selecting this option
+            />
+            kosher
+          </label>
+        </fieldset>
       </form>
     </section>
   );
